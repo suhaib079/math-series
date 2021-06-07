e1c8d65d68462f25476463312b2ccde8eaf2bcd3
suhaib079 <suhaibabdennabi33@gmail.com>
2021-06-07 17:47:35 +0300

    J

---
+++ math_series/series.py
@@ -0,0 +1,35 @@
+def fibonacci(n):
+    if n <= 0:
+        if n == 0:
+            return 0
+        else:
+            return 1
+    
+    return fibonacci(n-1) + fibonacci(n-2)
+
+
+
+def lucas(n):
+    
+    if n <= 1:
+        if n == 0:
+            return 2
+        else:
+            return 1
+
+    return lucas(n-1) + lucas(n-2)
+
+print(lucas(5))
+
+
+
+def sum_series(n,x=0,y=1):
+    
+    if x == 2 and y == 1:
+        return lucas(n)
+    elif x == 0 and y == 1:
+        return fibonacci(n)
+
+    else:
+        return fibonacci(n) + lucas(x)
+
---
+++ tests/test_series.py
@@ -0,0 +1,53 @@
+from math_series import __version__
+from math_series.series import fibonacci
+from math_series.series import lucas
+from math_series.series import sum_series
+
+def test_version():
+    assert __version__ == '0.1.0'
+
+
+
+# testing fibonacci
+
+def test_fibonacci():
+    expected =3
+    actual = fibonacci(4)
+    assert actual== expected
+
+def test_fibonacci2():
+    expected =8
+    actual = fibonacci(6)
+    assert actual== expected
+
+def test_fibonacci3():
+    expected =13
+    actual = fibonacci(7)
+    assert actual== expected
+
+
+    # testing  locus 
+
+def test_lucas():
+    expected = 11
+    actual = lucas(5)
+    assert actual == expected
+
+def test_lucas2():
+    expected = 18
+    actual = lucas(6)
+    assert actual == expected
+
+def test_lucas3():
+    expected = 7
+    actual = lucas(4)
+    assert actual == expected
+
+
+
+
+def test_sum_series():
+    assert sum_series(2) == 1
+
+
+
